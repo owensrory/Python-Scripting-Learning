90f87528154a0760d9b1be9ae37f299bbce7aae5
Rory <40439757@live.napier.ac.uk>
2022-01-29 22:57:22 +0000

    Conditionals and Comparisons in python

---
+++ conditionals_comparisons.py
@@ -0,0 +1,24 @@
+# Conditionals and Comparisons
+
+number = 2 in [1, 2, 3]
+print(number)
+
+if 1 == 8:
+		print('this is true')
+else:
+		print('this is false')
+
+name = 'Rory'
+
+if len(name) >= 6:
+	print('name is long')
+elif len(name) == 4:
+	print('name is perfect length')
+else:
+	print('name too short')
+
+
+print('SOMETHING'.lower())
+
+print(4*'1.1')
+print(2/3)
