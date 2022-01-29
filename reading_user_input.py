d5f559967f23c99a8432ee38b3e35d42438ac492
Rory <40439757@live.napier.ac.uk>
2022-01-29 23:01:37 +0000

    Basic reading and outputting user input

---
+++ reading_user_input.py
@@ -0,0 +1,10 @@
+name = input('What is your name?:	')
+birthdate = input('What is your date of birth?:		')
+age = int(input('How old are you?:    '))
+
+
+print(name + ' ' + birthdate + ' ' + str(age))
+
+
+
+
